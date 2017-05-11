d46c68d7a148576fdf4a185a1963ab0d501bdc1f
emma <emmahelenalindgren@gmail.com>
2017-05-11 10:30:19 +0200

    igen

--- loginScript.jsx
+++ loginScript.jsx
@@ -1,90 +1,95 @@
 
-var mapRoot = document.getElementById('mapRoot'),
-	logoutButton = document.getElementById('logoutButton');
-
-
+var mapRoot = document.getElementById('mapRoot');
 var updateUser;
-
 class LoginApp extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            user: firebase.auth().currentUser,
+            user: null,
             userProfileUrl:'',
+            userObj: {}
         };
         //bind us together forever and ever
         this.loginFunction = this.loginFunction.bind(this);
         this.googleFunction = this.googleFunction.bind(this);
+        /*this.logoutFunction = this.logoutFunction.bind(this);*/
     }
-loginFunction() {
-	console.log('this:',this)
-	let self = this;
-	let provider = new firebase.auth.FacebookAuthProvider();
-	firebase.auth().signInWithPopup(provider).then(function (result) {
-		//console.log(result.user.displayName);
-		 self.setState({
-			user: result.user,
-			 userProfileUrl : firebase.auth().currentUser.providerData[0].photoURL
-		})
-		localStorage.setItem("currentUserName", result.user.displayName);
-		localStorage.setItem("currentUserURL", result.user.photoURL);
-
-	}.bind(this)).catch(function (error) {
-	console.log("Login error")
-	alert("Kunde inte logga in ," + error);
-});
-};
+    loginFunction() {
+        console.log('this:',this)
+        let self = this;
+        let provider = new firebase.auth.FacebookAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            //console.log(result.user.displayName);
+             self.setState({
+                user: result.user,
+                 userProfileUrl : firebase.auth().currentUser.providerData[0].photoURL
+            })
+            //// end checker
+            //console.log('Email: ' , self.state.user.email)
+            //console.log('DisplayName: ', self.state.user)
+            //console.log('userProfilePic', self.state.userProfileUrl )
+            
+            localStorage.setItem("currentUserName", result.user.displayName);
+            localStorage.setItem("currentUserURL", result.user.photoURL);
+            //console.log(localStorage.getItem('currentUserObj'));
+            /*
+            console.log(obj);
+            this.setState({
+                userObj: obj
+            });*/
+        }.bind(this)).catch();
+    };
 googleFunction() {
 let self = this;
-	let googleProvider = new firebase.auth.GoogleAuthProvider();
-	firebase.auth().signInWithPopup(googleProvider).then(function (result) {
-		
-		self.setState({
-		user : result.user,
-		userProfileUrl : firebase.auth().currentUser.providerData[0].photoURL
-		})
-		localStorage.setItem("currentUserName", result.user.displayName);
-		localStorage.setItem("currentUserURL", result.user.photoURL);
-		console.log('DisplayName: ', self.state.user)
-			console.log('userProfilePic', self.state.userProfileUrl )
-	}).catch(function (error) {
-		console.log("Login error")
-		alert("Kunde inte logga in ," + error);
-	})
+    let googleProvider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(googleProvider).then(function (result) {
+        
+        self.setState({
+        user : result.user,
+        userProfileUrl : firebase.auth().currentUser.providerData[0].photoURL
+        })
+        console.log('DisplayName: ', self.state.user)
+            console.log('userProfilePic', self.state.userProfileUrl )
+    });
 };
-
 updateDisplay(){
-	mapRoot.style.display = 'block';
-	logoutButton.style.display = 'block';
-	
-	//console.log(isLoggedIn);
+    mapRoot.style.display = 'block';
+    //console.log(isLoggedIn);
 }
-	
-
-
-
+/*
+logoutFunction() {
+    firebase.auth().signOut().then(function (result) {
+        console.log("You are no more my friend");
+    }).catch(function (error) {
+        // Utloggning misslyckades
+        console.log("Det gick inte som vi ville" + error);
+        nameHolder.innerHTML = "Somting went wrong";
+    });
+};
+*/
 render() {
-	let loginComp;
-	if(this.state.user){ // If user is online
-		loginComp =<p id="nameHolder">Du är nu inloggad</p>;
-		console.log("Inloggad, Gå vidare till sidan")
-		//<button id=logoutButton" onClick={this.logoutFunction}>Logout</button>
-		//mapRoot.style.display = 'block';
-		this.updateDisplay();
-		initMap();
-		//console.log(updateUser);
-		localStorage.setItem("currentUserPicutre", this.state.userProfileUrl);
-		
-	} else {
-		// User not signed in.
-		console.log("Ej inloggad, Meddelande till användare on inloggning")
-		loginComp = <div><p id="nameHolder">Please login with Facebook or Google</p><button className="loginBtn loginBtn--facebook" id="loginButton" onClick={this.loginFunction} > Login with Facebook </button><button className="loginBtn loginBtn--google" id="googleLogin" onClick={this.googleFunction}> Login with Google </button> </div>	
-	}
-	console.log("current user from firebase ",firebase.auth().currentUser);
-
+    let loginComp;
+    if(this.state.user){ // If user is online
+        loginComp =<p id="nameHolder">Du är nu inloggad</p>;
+        console.log("Inloggad, Gå vidare till sidan")
+        //<button id=logoutButton" onClick={this.logoutFunction}>Logout</button>
+        //mapRoot.style.display = 'block';
+        this.updateDisplay();
+        initMap();
+        //console.log(updateUser);
+        localStorage.setItem("currentUserPicutre", this.state.userProfileUrl);
+        //console.log(this.state.userObj);
+        
+        
+    } else {
+        // User not signed in.
+        console.log("Ej inloggad, Meddelande till användare on inloggning")
+        loginComp = <div><p id="nameHolder"></p><button className="loginBtn loginBtn--facebook" id="loginButton" onClick={this.loginFunction} >Logga in med Facebook <i className="fa fa-facebook fa-lg" aria-hidden="true"></i></button><br /> eller <br /><button className="loginBtn loginBtn--google" id="googleLogin" onClick={this.googleFunction}> Logga in med Google <i className="fa fa-google-plus fa-lg" aria-hidden="true"></i></button> </div>  
+    }
     return (
     <div id="divContaienr">
-			{loginComp}
+            {loginComp}
+        
     </div>
         )
     }
