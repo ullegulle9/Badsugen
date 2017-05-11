5c25d245fcf54599d4166906e7856787d359138f
Martin Larsson <martin.larsson.post@gmail.com>
2017-05-11 14:33:06 +0200

    sort + css

--- rating.js
+++ rating.js
@@ -64,6 +64,7 @@ class FormComponent extends React.Component {
 	  this.updateCurrentObj = this.updateCurrentObj.bind(this);
 	  this.clickList = this.clickList.bind(this);
 	  this.handleEdit = this.handleEdit.bind(this);
+	  this.sortChange = this.sortChange.bind(this);
  }
   resetStars(){
      this.setState({
@@ -268,13 +269,41 @@ class FormComponent extends React.Component {
 
 		this.showCommentsAndRating();
 	}
+	
+	sortChange(ev){
 
+		console.log(this.state.currentObjId);
+		console.log(ev.target.value);
+	let fb = firebase.database();	fb.ref(`omdömen/${this.state.currentObjId}/rateComment/`).orderByChild(ev.target.value).once('value', snap => {
+			let sorted = [];
+		snap.forEach(o =>{
+			sorted.push(o.val());
+		});
+		this.setState({
+			commentList: sorted.reverse()
+		});
+		
+		});
+	}
+
+	/*<li>
+         <i id="changeComment" class="fa fa-pencil-square-o fa-5" aria-hidden="true"></i><img src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-1/p160x160/13256253_10154096534105792_6902039832411105859_n.jpg?oh=cb5c804b5fc34ca34451d80448526c05&oe=59B8A842" alt="profilbild" id="">
+        <p id="NameComment">Martin Larsson <span id="posted">Postat 2017-05-11 10:57:05</span></p>
+        <p class="writtenComment">"Så himla bra badplats! Så himla bra badplats! Så himla bra badplats!"</p>
+        <p id="rating">Betyg: 5</p>
+       
+    </li>*/
 
  render() {
    currentObj2 = this.updateCurrentObj;
 	 let elementList = this.state.commentList.map(el => {
-		 return <li key={el.time} id={el.time} onClick={this.clickList} >{el.comment}. Betyg: {el.rating} <br></br>
-		 {el.userName}, postat {el.time} <img src={el.userURL}></img> </li>
+		 return <li key={el.time} id={el.time}>
+			 <i className="fa fa-pencil-square-o fa-5 changeComment" aria-hidden="true" onClick={this.clickList}></i>
+			 <img src={el.userURL} alt="profilbild"/>
+			 <p className="NameComment">{el.userName} <span className="posted">Postat {el.time}</span></p>
+			 <p className="writtenComment">{el.comment} </p>
+			 <p className="rating">Betyg: {el.rating}</p>
+			  </li>
 	 });
 	 //console.log(this.state.clickedID);
 	 if (this.state.clickedID === null) {
@@ -291,7 +320,12 @@ class FormComponent extends React.Component {
 		 </div>
      <div>
      <input value={this.state.comment} onChange={this.handleInput} type="text" placeholder="Kommentarer..." />
-     <button  onClick={this.handleSubmit}  type="button">Skicka</button>
+     <button  onClick={this.handleSubmit}  type="button">Skicka</button><br></br><h3>Kommentarer</h3>
+		 <span>Sortera efter</span>
+		 <select onChange={this.sortChange}>
+			 <option value="rating">Betyg</option>
+			 <option value="time">Datum</option>
+		 </select>
      </div>
 		   <ul>{elementList}</ul>
      </div>
